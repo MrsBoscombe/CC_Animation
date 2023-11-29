66d3fd7bb8aa7777ecaac8e9eb3fee90cba2dbec
Mrs. Boscombe <30632430+MrsBoscombe@users.noreply.github.com>
2023-11-28 19:39:27 -0500

    Initial Commit

---
+++ Assets/CreativeCore_Animation/Scripts/DoorTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("PlayerProximity");
+        Debug.Log("door trigger triggered");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetTrigger("PlayerProximity");
+    }
+}
